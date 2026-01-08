1d8cc91bcd0814b490f4dbd3317b2e12a0c8b375
lakshmi-jainapally-hod <varalakshmi.jainapally@digital.homeoffice.gov.uk>
2026-01-08 09:30:24 +0000

    update tags

--- alerts.tf
+++ alerts.tf
@@ -56,25 +56,15 @@ resource "aws_kms_key" "sns_canary_cmk" {
           "kms:GenerateDataKey"
         ]
         Resource = "*"
-      },
-      # CloudWatch Alarms (required)
-      {
-        Sid    = "AllowCloudWatchToUseKey"
-        Effect = "Allow"
-        Principal = {
-          Service = "cloudwatch.amazonaws.com"
-        }
-        Action = [
-          "kms:Encrypt",
-          "kms:Decrypt",
-          "kms:GenerateDataKey"
-        ]
-        Resource = "*"
       }
     ]
   })
 
   tags = local.tags
+}
+resource "aws_kms_alias" "cw_logs_alias" {
+  name          = "alias/${var.environment}-cwlogs-slack-forwarder"
+  target_key_id = aws_kms_key.cw_logs_cmk.key_id
 }
 
 # KMS Alias
@@ -92,13 +82,11 @@ resource "aws_sns_topic" "canary_alerts" {
     Version = "2012-10-17"
     Statement = [
       {
-        Sid    = "AllowPublishFromCloudWatchAlarms"
-        Effect = "Allow"
-        Principal = {
-          Service = "cloudwatch.amazonaws.com"
-        }
-        Action   = "sns:Publish"
-        Resource = "arn:aws:sns:${data.aws_region.current.region}:${data.aws_caller_identity.current.account_id}:${var.environment}-canary-alerts"
+        Sid       = "AllowPublishFromCloudWatchAlarms"
+        Effect    = "Allow"
+        Principal = { Service = "cloudwatch.amazonaws.com" }
+        Action    = "sns:Publish"
+        Resource  = aws_sns_topic.canary_alerts.arn
         Condition = {
           StringEquals = {
             "AWS:SourceAccount" = data.aws_caller_identity.current.account_id
@@ -110,7 +98,6 @@ resource "aws_sns_topic" "canary_alerts" {
 
   tags = local.tags
 }
-
 # Lambda IAM Role
 resource "aws_iam_role" "slack_lambda_role" {
   name = "${var.environment}-slack-forwarder-role"
@@ -156,14 +143,63 @@ data "archive_file" "slack_zip" {
 }
 
 # Lambda function
+resource "aws_signer_signing_profile" "slack_forwarder" {
+  name_prefix = "${var.environment}-slack-forwarder-"
+  platform_id = "AWSLambda-SHA384-ECDSA"
+  tags        = local.tags
+}
+
+resource "aws_lambda_code_signing_config" "slack_forwarder" {
+  allowed_publishers {
+    signing_profile_version_arns = [aws_signer_signing_profile.slack_forwarder.arn]
+  }
+
+  policies {
+    untrusted_artifact_on_deployment = "Enforce"
+  }
+
+  tags = local.tags
+}
+resource "aws_sqs_queue" "slack_forwarder_dlq" {
+  name                      = "${var.environment}-slack-forwarder-dlq"
+  message_retention_seconds = 1209600 # 14 days
+  kms_master_key_id         = aws_kms_key.cw_logs_cmk.arn
+  tags                      = local.tags
+}
+resource "aws_iam_role_policy" "slack_forwarder_dlq_send" {
+  name = "${var.environment}-slack-forwarder-dlq-send"
+  role = aws_iam_role.slack_lambda_role.name
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect   = "Allow"
+        Action   = ["sqs:SendMessage"]
+        Resource = aws_sqs_queue.slack_forwarder_dlq.arn
+      }
+    ]
+  })
+}
+
+resource "aws_cloudwatch_log_group" "slack_forwarder" {
+  name              = "/aws/lambda/${aws_lambda_function.slack_forwarder.function_name}"
+  kms_key_id        = aws_kms_key.cw_logs_cmk.arn
+  retention_in_days = 365
+  tags              = local.tags
+}
+
 resource "aws_lambda_function" "slack_forwarder" {
-  filename      = data.archive_file.slack_zip.output_path
-  function_name = "${var.environment}-slack-forwarder"
-  handler       = "slack_forwarder.lambda_handler"
-  role          = aws_iam_role.slack_lambda_role.arn
-  runtime       = "python3.11"
-  timeout       = 10
-  memory_size   = 128
+  filename                       = data.archive_file.slack_zip.output_path
+  function_name                  = "${var.environment}-slack-forwarder"
+  handler                        = "slack_forwarder.lambda_handler"
+  role                           = aws_iam_role.slack_lambda_role.arn
+  kms_key_arn                    = aws_kms_key.cw_logs_cmk.arn
+  runtime                        = "python3.11"
+  timeout                        = 10
+  memory_size                    = 128
+  reserved_concurrent_executions = 2
+  code_signing_config_arn        = aws_lambda_code_signing_config.slack_forwarder.arn
 
   environment {
     variables = var.slack_secret_arn != "" ? {
@@ -172,9 +208,21 @@ resource "aws_lambda_function" "slack_forwarder" {
       SLACK_WEBHOOK_URL = var.slack_webhook_url
     }
   }
+  vpc_config {
+    subnet_ids         = var.subnet_ids
+    security_group_ids = var.security_group_ids
+  }
 
   source_code_hash = fileexists(data.archive_file.slack_zip.output_path) ? filebase64sha256(data.archive_file.slack_zip.output_path) : null
   tags             = local.tags
+  depends_on       = [aws_cloudwatch_log_group.slack_forwarder]
+
+  tracing_config {
+    mode = "Active"
+  }
+  dead_letter_config {
+    target_arn = aws_sqs_queue.slack_forwarder_dlq.arn
+  }
 }
 
 # SNS → Lambda subscription
@@ -182,6 +230,8 @@ resource "aws_sns_topic_subscription" "lambda_sub" {
   topic_arn = aws_sns_topic.canary_alerts.arn
   protocol  = "lambda"
   endpoint  = aws_lambda_function.slack_forwarder.arn
+
+  depends_on = [aws_lambda_permission.allow_sns]
 }
 
 resource "aws_lambda_permission" "allow_sns" {
