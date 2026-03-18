f89e89dc1eac5df9ce64fa296005726601cc1fdc
lakshmi-jainapally-hod <varalakshmi.jainapally@digital.homeoffice.gov.uk>
2026-03-18 16:16:51 +0000

    added provider

--- main.tf
+++ main.tf
@@ -1,3 +1,15 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+provider "aws" {
+  region = var.region
+}
+
 resource "aws_s3_bucket" "canary_bucket" {
   bucket        = var.bucket_name
   force_destroy = true
@@ -223,12 +235,4 @@ resource "aws_synthetics_canary" "vpc_connectivity" {
   }
 
   tags = local.tags
-}
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
 }
